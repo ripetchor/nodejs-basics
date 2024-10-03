d7356d87aca1656f17b48b78c199ee0e8a6f3287
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-03 20:36:15 +0500

    feat: add solutions for zip/compress.js and zip/decompress.jz

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { resolve } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+import { throwError } from '../helpers.js';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const sourcePath = resolve(import.meta.dirname, 'files', 'fileToCompress.txt');
+    const destinationPath = resolve(import.meta.dirname, 'files', 'archive.gz');
+
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destinationPath);
+
+    await pipeline(readStream, createGzip(), writeStream);
+  } catch (error) {
+    throwError(error.message);
+  }
 };
 
 await compress();
