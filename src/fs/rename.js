9eb4d564cc319a559f4597df444ce3205709180b
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-03 17:02:14 +0500

    feat: add solution for fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fsPromises from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { exists, throwError } from '../helpers.js';
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const oldFilePath = resolve(import.meta.dirname, 'files', 'wrongFilename.txt');
+    const newFilePath = resolve(import.meta.dirname, 'files', 'properFilename.md');
+
+    const oldFileExists = await exists(oldFilePath);
+    const newFileExists = await exists(newFilePath);
+
+    if (!oldFileExists || newFileExists) {
+      throwError();
+    }
+
+    await fsPromises.rename(oldFilePath, newFilePath);
+  } catch {
+    throwError();
+  }
 };
 
 await rename();
