a8ca1d949357c6d562ae93ff5ff34603d01e8902
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-03 17:02:53 +0500

    feat: add solution for fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { rm } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { exists, throwError } from '../helpers.js';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const targetPath = resolve(import.meta.dirname, 'files', 'fileToRemove.txt');
+
+    const fileExists = await exists(targetPath);
+
+    if (!fileExists) {
+      throwError();
+    }
+
+    await rm(targetPath);
+  } catch {
+    throwError();
+  }
 };
 
 await remove();
