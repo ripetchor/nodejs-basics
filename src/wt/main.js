982201fde13301ffac0748c9ef9c7ce3b70b26b9
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-03 22:17:25 +0500

    feat: add solution for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { cpus } from 'node:os';
+import { resolve } from 'node:path';
+import { isMainThread, Worker } from 'node:worker_threads';
+import { throwError } from '../helpers.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  try {
+    if (isMainThread) {
+      const workerPath = resolve(import.meta.dirname, 'worker.js');
+
+      let n = 10;
+
+      const results = [];
+
+      for (let i = 0; i < cpus().length; i += 1) {
+        const worker = new Worker(workerPath, { workerData: n });
+
+        n += 1;
+
+        const promise = new Promise((resolve) => {
+          worker.on('message', (message) => {
+            resolve({ status: 'resolved', data: message });
+          });
+
+          worker.on('error', () => {
+            resolve({ status: 'error', data: null });
+          });
+        });
+
+        results.push(promise);
+      }
+
+      console.log(await Promise.all(results));
+    }
+  } catch (error) {
+    throwError(error.message);
+  }
 };
 
 await performCalculations();
