850f5755cd016b5b53f305d58a87d29431c9a92b
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-03 17:34:25 +0500

    feat: add solution for streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'node:fs';
+import { resolve } from 'node:path';
+import { stdin } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+import { throwError } from '../helpers.js';
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const destinationFilePath = resolve(import.meta.dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = createWriteStream(destinationFilePath, { flags: 'a+' });
+
+    await pipeline(stdin, writeStream);
+  } catch (error) {
+    throwError(error.message);
+  }
 };
 
 await write();
