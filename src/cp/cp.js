5cc0467aa926631e40010755b1641cfd7f69dc8b
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-04 20:00:34 +0500

    feat: add solution for cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
+import { fork } from 'node:child_process';
+import { EOL } from 'node:os';
+import { resolve } from 'node:path';
+import { stdout } from 'node:process';
+import { throwError } from '../helpers.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const modulePath = resolve(import.meta.dirname, 'files', 'script.js');
+
+  const childProcess = fork(modulePath, args);
+
+  childProcess.on('error', (error) => {
+    throwError(error.message);
+  });
+
+  childProcess.on('exit', (code) => {
+    stdout.write('Child process exit with code: ' + code + EOL);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([
+  'someArgument1',
+  'someArgument2',
+  'someArgument3',
+  'someArgument4',
+  'someArgument5',
+]);
