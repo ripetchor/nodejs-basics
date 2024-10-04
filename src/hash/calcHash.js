548a0c90e6ad223fc24346d6baff7515811eb269
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-04 20:19:48 +0500

    feat: add solution for hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'node:fs';
+import { EOL } from 'node:os';
+import { resolve } from 'node:path';
+import { stdout } from 'node:process';
+import { throwError } from '../helpers.js';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const filePath = resolve(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const readStream = createReadStream(filePath);
+
+    const hash = createHash('sha256');
+
+    readStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+      stdout.write(hash.digest('hex') + EOL);
+    });
+  } catch (error) {
+    throwError(error.message);
+  }
 };
 
 await calculateHash();
